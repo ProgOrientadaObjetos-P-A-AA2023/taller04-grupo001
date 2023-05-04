e3771d205bffadb318bcdd230d1a616e3e3d2b83
Italito27 <iilopez1@utpl.edu.ec>
2023-05-04 10:59:47 -0500

    iiii

---
+++ problema1/problema1/src/problema1/Ejecutor.java
@@ -0,0 +1,17 @@
+
+package problema1;
+
+/**
+ *
+ * @author Italo
+ */
+public class Ejecutor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
